3d0d66d59893cc575082760aec82178fa539e784
iddey <iddengo@protonmail.com>
2026-02-04 16:42:00 +0100

    refactor: remove unused items from wrapper traits

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -45,16 +45,6 @@ where
     D: DrawTarget,
     D::Error: Debug,
 {
-    fn call(&mut self, f: impl FnMut(&mut D) -> Result<(), D::Error>) -> Result<(), D::Error> {
-        self.inner_mut().call(f)
-    }
-
-    fn draw_hidden<'z, I>(&mut self, content: I) -> Result<(), Self::Error>
-    where
-        I: Iterator<Item = (u16, u16, &'z Cell)>,
-    {
-        self.inner_mut().draw_hidden(content)
-    }
 }
 
 pub trait ConfigureBackendWrapper<'a, 'b, 'c, B, T, C>: Wrapper<Inner = B>
@@ -64,53 +54,6 @@ where
     T: PixelColor + Default + Invert + Screen + WeightedAvg + From<Rgb888>,
     RawDataSlice<'a, C::Raw, BigEndian>: IntoIterator<Item = C::Raw>,
 {
-    fn font(&self) -> &'b BitmapFont<'a, C, 1> {
-        self.inner().font()
-    }
-
-    fn set_font(&mut self, font: &'b BitmapFont<'a, C, 1>) {
-        self.inner_mut().set_font(font);
-    }
-
-    fn font_bold(&self) -> Option<&'b BitmapFont<'a, C, 1>> {
-        self.inner().font_bold()
-    }
-
-    fn set_font_bold(&mut self, font_bold: Option<&'b BitmapFont<'a, C, 1>>) {
-        self.inner_mut().set_font_bold(font_bold);
-    }
-
-    fn fg_reset(&self) -> Option<T> {
-        self.inner().fg_reset()
-    }
-
-    fn set_fg_reset(&mut self, fg_reset: Option<T>) {
-        self.inner_mut().set_fg_reset(fg_reset);
-    }
-
-    fn bg_reset(&self) -> Option<T> {
-        self.inner().bg_reset()
-    }
-
-    fn set_bg_reset(&mut self, bg_reset: Option<T>) {
-        self.inner_mut().set_bg_reset(bg_reset);
-    }
-
-    fn palette(&self) -> Palette<'c, T> {
-        self.inner().palette()
-    }
-
-    fn set_palette(&mut self, palette: Palette<'c, T>) {
-        self.inner_mut().set_palette(palette);
-    }
-
-    fn anchor_x(&self) -> AnchorX {
-        self.inner().anchor_x()
-    }
-
-    fn set_anchor_x(&mut self, anchor_x: AnchorX) {
-        self.inner_mut().set_anchor_x(anchor_x);
-    }
 }
 
 impl<W, B, D> DrawTargetBackend<D> for W
